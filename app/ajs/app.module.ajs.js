b1574caae4d39bfc38fa7b0d27695d061b989340
pehu71 <humplikpetr@gmail.com>
2017-02-08 22:53:29 +0100

    delete events

--- app/ajs/app.module.ajs.js
+++ app/ajs/app.module.ajs.js
@@ -7,21 +7,25 @@ angular.module('hybridApp', ['list', 'book-service'])
         ctrl.eventList = [];
         ctrl.books = [];
 
+        /* this is the callback fn passed to the <list-component> directive in index.html */
         ctrl.onSelected = function (event) {
             ctrl.eventList.push(event);
         };
 
+        /* this is the callback fn passed to the <list-component> directive in index.html */
         ctrl.onDeleteQuery = function (event) {
             event['showButtons'] = true;
             ctrl.eventList.push(event);
         };
 
+        /* this is the callback fn passed to the <header-component> directive in index.html */
         ctrl.doRollback = function (event) {
             event['showButtons'] = false;
             var newEvent = {eventType:'delete rollback', itemId: event.itemId, itemName: event.itemName};
             ctrl.eventList.push(newEvent);
         };
 
+        /* this is the callback fn passed to the <header-component> directive in index.html */
         ctrl.doCommit = function (event) {
             event['showButtons'] = false;
             var newEvent = {eventType:'delete commit', itemId: event.itemId, itemName: event.itemName};
@@ -35,6 +39,12 @@ angular.module('hybridApp', ['list', 'book-service'])
             ctrl.books.splice(index, 1);
         };
 
+        /* this is the callback fn passed to the <header-component> directive in index.html */
+        ctrl.doClearEvents = function () {
+            ctrl.eventList = []
+        };
+
+        /* $onInit is a safe place to fetch data */
         this.$onInit = function () {
             bookService.getAll().then(
                 function (res) {
@@ -49,18 +59,30 @@ angular.module('hybridApp', ['list', 'book-service'])
         bindings: {
             eventList: '<',
             onRollback: '&',
-            onCommit: '&'
+            onCommit: '&',
+            onClearEvents: '&'
         },
 
         controller: function () {
             var self = this;
 
+            /* self.rollback is called from directive template */
             self.rollback = function (e) {
-                self.onRollback({item: e});
+                /* onRollback is exposed as a directive output parameter
+                   - see the bindings object
+                   - see the directive used in index.html
+                   - the callback fn which is passed tot this attribute must use the name "item"
+                     to catch the emitted object, see index.html
+                */
+                self.onRollback({item: e})
             };
 
             self.commit = function (e) {
-                self.onCommit({item: e});
+                self.onCommit({item: e})
+            };
+
+            self.clearEvents = function () {
+                self.onClearEvents()
             }
         },
 
